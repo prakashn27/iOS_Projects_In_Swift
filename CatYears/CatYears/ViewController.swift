0dee003e8336f012b424c07571edc3315e4d717b
prakashn27 <nprakashece@gmail.com>
2014-12-24 15:31:59 -0500

    added the cat Years Project

---
+++ CatYears/CatYears/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  CatYears
+//
+//  Created by Prakash on 12/24/14.
+//  Copyright (c) 2014 prakashn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var LabelResult: UILabel!
+    @IBOutlet weak var TextFieldCatAge: UITextField!
+    
+    @IBAction func FindAge(sender: AnyObject) {
+        var enteredAge = TextFieldCatAge.text.toInt()
+        println("printing age = \(enteredAge)")
+        var catYears = enteredAge! * 7  //! - dont worry that it is sure that entered age is a value
+        LabelResult.text = String(catYears)
+        //println("cat's age = \(age * 7)")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
+
